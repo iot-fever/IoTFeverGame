248fc5ee0e258a1d80b3f8c910b3c4452f80fd27
andreasfoitzik <andreas.foitzik@live.com>
2015-09-09 19:24:40 +0800

    Sensor Works

---
+++ IoTFeverGame/SensorTagInitializationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SensorTagInitializationViewController.swift
+//  IoTFeverGame
+//
+//  Created by Foitzik Andreas on 9/9/15.
+//  Copyright (c) 2015 Foitzik Andreas. All rights reserved.
+//
+
+import Cocoa
+
+class SensorTagInitializationViewController: UIViewController {
+
+}
