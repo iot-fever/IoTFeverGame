ea0a005612da89a7b868758304a87dddf0640d13
andreasfoitzik <andreas.foitzik@live.com>
2015-09-09 11:47:34 +0800

    Changes in the game logic

---
+++ IoTFeverGame/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  IoTFeverGame
+//
+//  Created by Foitzik Andreas on 8/9/15.
+//  Copyright (c) 2015 Foitzik Andreas. All rights reserved.
+//
+
+import Foundation
---
+++ IoTFeverGame/SensorReader.swift
@@ -0,0 +1,9 @@
+//
+//  SensorReader.swift
+//  IoTFeverGame
+//
+//  Created by Foitzik Andreas on 8/9/15.
+//  Copyright (c) 2015 Foitzik Andreas. All rights reserved.
+//
+
+import Foundation
