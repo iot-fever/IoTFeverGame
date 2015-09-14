635b9e9ee11ac0adbcc5ea2c3d387db82ee84b45
Zubair Hamed <zubairhamed@gmail.com>
2015-09-15 00:44:16 +0800

    Discoidal Animated Intro Screen

---
+++ IoTFeverGame-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  IoTFeverGame-Bridging-Header.h
+//  IoTFeverGame
+//
+//  Created by Zubair Hamed on 15/9/15.
+//  Copyright (c) 2015 Foitzik Andreas. All rights reserved.
+//
+
+#import "UIImage+animatedGIF.h"
--- IoTFeverGame/ViewController.swift
+++ IoTFeverGame/ViewController.swift
@@ -13,13 +13,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var playerNameText   : UITextField!
     @IBOutlet weak var playButton       : UIButton!
+    var discoBall : UIImageView = UIImageView()
     
     override func viewDidLoad() {
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         playButton.enabled = true
         
+        let url = NSBundle.mainBundle().URLForResource("disco-anim", withExtension: "gif");
+        let gif = UIImage.animatedImageWithAnimatedGIFURL(url)
+        
+        let screenSize = UIScreen.mainScreen().bounds
+        discoBall.image = gif
+        discoBall.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+
+        self.view.addSubview(discoBall)
+        self.view.sendSubviewToBack(discoBall)
+        self.view.backgroundColor = UIColor.brownColor()
+        
+        super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
