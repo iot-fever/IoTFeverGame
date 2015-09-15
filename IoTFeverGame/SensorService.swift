3cc6475b20ab3d7ac2097dc18b5743477a8e316b
Kenny Lee <shevken@gmail.com>
2015-09-15 10:37:37 +0800

    Re-organise the class.

--- IoTFeverGame/SensorService.swift
+++ IoTFeverGame/SensorService.swift
@@ -9,9 +9,11 @@
 import Foundation
 import CoreBluetooth
 
+// The system info of the 2 sensors
 let sensorLeftID = "808b70000084bec4"
 let sensorRightID = "8088b9000048b4b0"
 
+// The UUID of the 2 sensors
 let sensorLeftUUID = "66CB73B3-8E80-BC0F-8D22-69991491A33E"
 let sensorRightUUID = "516AB7BD-7B80-285A-B652-20CE104FC6BF"
 
@@ -22,17 +24,11 @@ protocol IOTFeverDataAware {
     func onDataIncoming(data: [Double])
 }
 
-
 class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
     /******* CBCentralPeripheralDelegate *******/
     
     // Check if the service discovered is valid i.e. one of the following:
-    // IR Temperature Service
-    // Accelerometer Service
-    // Humidity Service
-    // Magnetometer Service
-    // Barometer Service
-    // Gyroscope Service
+    // Movement Service
     // (Others are not implemented)
     
     var sensorTagPeripheralLeft : CBPeripheral!
@@ -40,7 +36,6 @@ class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
 
     var sensorLeftFound = false
     var sensorRightFound = false
-
     
     var subscribers = [IOTFeverDataAware]()
     override init() {
@@ -56,12 +51,67 @@ class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
             vc.onDataIncoming(data)
         }
     }
-    
-    
-    
-    
+
     func sensorsFound() -> Bool {
         return sensorLeftFound && sensorRightFound
+    }
+    
+    // Check status of BLE hardware
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        if central.state == CBCentralManagerState.PoweredOn {
+            // Scan for peripherals if BLE is turned on
+            central.scanForPeripheralsWithServices(nil, options: nil)
+            println("Searching for BLE Devices")
+        }
+        else {
+            // Can have different conditions for all states if needed - show generic alert for now
+            println("Bluetooth switched off or not initialized")
+        }
+    }
+    
+    // Check out the discovered peripherals to find Sensor Tag
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        
+        var deviceUUID  = peripheral.identifier.UUIDString
+        
+        if SensorReader.sensorTagFound(advertisementData) == true {
+            
+            println("Scanning...Detected \(deviceUUID)")
+            
+            if sensorLeftUUID == deviceUUID {
+                self.sensorTagPeripheralLeft = peripheral
+                self.sensorTagPeripheralLeft.delegate = self
+                println("Left Sensor Found")
+                sensorLeftFound = true
+            }
+                
+            else if sensorRightUUID == deviceUUID {
+                self.sensorTagPeripheralRight = peripheral
+                self.sensorTagPeripheralRight.delegate = self
+                println("Right Sensor Found")
+                sensorRightFound = true
+            }
+            
+            central.connectPeripheral(peripheral, options: nil)
+        }
+        
+        if sensorsFound() {
+            println("Left and Right sensors found.")
+            central.stopScan()
+        }
+    }
+    
+    // Discover services of the peripheral
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        println("Discovering peripheral services")
+        peripheral.discoverServices(nil)
+    }
+    
+    
+    // If disconnected, start searching again
+    func centralManager(central: CBCentralManager!, didDisconnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+        println("Disconnected")
+        central.scanForPeripheralsWithServices(nil, options: nil)
     }
     
     // Check if the service discovered is valid i.e. one of the following:
@@ -77,7 +127,6 @@ class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
             }
         }
     }
-    
     
     // Enable notification and sensor for each characteristic of valid service
     func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
@@ -117,7 +166,6 @@ class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
         
         if characteristic.UUID == MovementDataUUID {
             let movementData = SensorReader.getMovementData(characteristic.value)
-            
             let accelData = SensorReader.getAccelerometerData(movementData)
             var accelerometerX = accelData[0]
             
@@ -132,75 +180,5 @@ class SensorDelegate: NSObject, CBPeripheralDelegate, CBCentralManagerDelegate {
             //self.publish(accelData)
         }
     }
-    
-    // Check status of BLE hardware
-    func centralManagerDidUpdateState(central: CBCentralManager!) {
-        if central.state == CBCentralManagerState.PoweredOn {
-            // Scan for peripherals if BLE is turned on
-            central.scanForPeripheralsWithServices(nil, options: nil)
-            println("Searching for BLE Devices")
-        }
-        else {
-            // Can have different conditions for all states if needed - show generic alert for now
-            println("Bluetooth switched off or not initialized")
-        }
-    }
-    
-    
-    // Check out the discovered peripherals to find Sensor Tag
-    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
-        
-        var deviceUUID  = peripheral.identifier.UUIDString
-        
-        if SensorReader.sensorTagFound(advertisementData) == true {
-            
-            println("Scanning...Detected \(deviceUUID)")
-            
-            if sensorLeftUUID == deviceUUID {
-                self.sensorTagPeripheralLeft = peripheral
-                self.sensorTagPeripheralLeft.delegate = self
-                println("Left Sensor Found")
-                sensorLeftFound = true
-            }
-                
-            else if sensorRightUUID == deviceUUID {
-                self.sensorTagPeripheralRight = peripheral
-                self.sensorTagPeripheralRight.delegate = self
-                println("Right Sensor Found")
-                sensorRightFound = true
-            }
-            
-            central.connectPeripheral(peripheral, options: nil)
-        }
-        
-        if sensorsFound() {
-            println("Left and Right sensors found.")
-            central.stopScan()
-        }
-        
-        
-        /*if SensorTag.sensorTagFound(advertisementData) == true {
-        // Update Status Label
-        self.statusLabel.text = "Sensor Tag Found"
-        
-        // Stop scanning, set as the peripheral to use and establish connection
-        //self.centralManager.stopScan()
-        self.sensorTagPeripheral = peripheral
-        self.sensorTagPeripheral.delegate = self
-        self.centralManager.connectPeripheral(peripheral, options: nil)
-        }*/
-    }
-    
-    // Discover services of the peripheral
-    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
-        println("Discovering peripheral services")
-        peripheral.discoverServices(nil)
-    }
-    
-    
-    // If disconnected, start searching again
-    func centralManager(central: CBCentralManager!, didDisconnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
-        println("Disconnected")
-        central.scanForPeripheralsWithServices(nil, options: nil)
-    }
+
 }
