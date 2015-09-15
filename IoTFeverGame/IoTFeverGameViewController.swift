57913721e5fb1ad97a5d0be4058ca2f8933e574b
andreasfoitzik <andreas.foitzik@live.com>
2015-09-15 11:23:22 +0800

    GET USER and POST HIGHSCORE requests

--- IoTFeverGame/IoTFeverGameViewController.swift
+++ IoTFeverGame/IoTFeverGameViewController.swift
@@ -49,7 +49,7 @@ class IoTFeverGameViewController: UIViewController, IOTFeverDataAware, AnyObject
     override func viewDidLoad() {
         super.viewDidLoad()
         // Subscriber
-        currentGame = IoTFeverGame(username:"Andreas")
+        currentGame = IoTFeverGame(username: username)
         let firstLevel = currentGame.start()
         self.timeCountLabel.text = String(firstLevel.duration)
         createNewMove(firstLevel)
@@ -100,6 +100,8 @@ class IoTFeverGameViewController: UIViewController, IOTFeverDataAware, AnyObject
     
     func renderGameOverSuccessful() {
         self.GameOver.text = "You won!"
+        //TODO replace with Publisher (Protocol)
+        PostHighscore(currentGame.player.score)
     }
     
     func renderGameOverUnsuccessful() {
@@ -115,6 +117,9 @@ class IoTFeverGameViewController: UIViewController, IOTFeverDataAware, AnyObject
         self.view.addSubview(hulk)
         self.view.bringSubviewToFront(hulk)
         self.GameOver.text = "You lost! Maybe next time."
+        
+        //TODO replace with Publisher (Protocol)
+        PostHighscore(currentGame.player.score)
     }
     
     func validateLastAndCreateNewMove() {
@@ -164,9 +169,7 @@ class IoTFeverGameViewController: UIViewController, IOTFeverDataAware, AnyObject
     func createNewMove(level : Level) {
         level.newMove()
         self.imageView.image = UIImage(named: level.currentMove!.getImage())
-        
     }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -209,5 +212,44 @@ class IoTFeverGameViewController: UIViewController, IOTFeverDataAware, AnyObject
   
     func levelCountdown() {
         self.timeCountLabel.text = String(currentGame.getCurrentLevel().duration--)
+    }
+    
+    func PostHighscore(score : Int){
+        println("Post Highscore")
+        
+        var postsUrlRequest = NSMutableURLRequest(URL: NSURL(string: "http://192.168.1.32:1337/highscore/vorto"
+)!)
+        postsUrlRequest.HTTPMethod = "POST"
+        
+        var newPost: NSDictionary = ["score": score];
+        var postJSONError: NSError?
+        var jsonPost = NSJSONSerialization.dataWithJSONObject(newPost, options: nil, error:  &postJSONError)
+        postsUrlRequest.HTTPBody = jsonPost
+        postsUrlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        NSURLConnection.sendAsynchronousRequest(postsUrlRequest, queue: NSOperationQueue(), completionHandler:{
+            (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            if let anError = error
+            {
+                // got an error in getting the data, need to handle it
+                println("error calling POST")
+            }
+            else
+            {
+                // parse the result as json, since that's what the API provides
+                var jsonError: NSError?
+                let post = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as! NSDictionary
+                if let aJSONError = jsonError
+                {
+                    // got an error while parsing the data, need to handle it
+                    println("error parsing response from POST")
+                }
+                else
+                {
+                    // we should get the post back, so print it to make sure all the fields are as we set to and see the id
+                    println("The post is: " + post.description)
+                }
+            }
+        })
+        
     }
 }
--- IoTFeverGame/ViewController.swift
+++ IoTFeverGame/ViewController.swift
@@ -8,17 +8,22 @@
 
 import UIKit
 
+var username: String = ""
+
 class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
-    @IBOutlet weak var playerNameText   : UITextField!
+    @IBOutlet weak var playerNameText   : UILabel!
     @IBOutlet weak var playButton       : UIButton!
+    
     var discoBall : UIImageView = UIImageView()
     
     override func viewDidLoad() {
         // Do any additional setup after loading the view, typically from a nib.
         
-        playButton.enabled = true
+        self.playButton.enabled = true
+        self.playerNameText.text = "Waiting for User ..."
+
         
         let url = NSBundle.mainBundle().URLForResource("disco-anim", withExtension: "gif");
         let gif = UIImage.animatedImageWithAnimatedGIFURL(url)
@@ -32,6 +37,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         self.view.backgroundColor = UIColor.brownColor()
         
         super.viewDidLoad()
+        
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("getUsername"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,11 +46,50 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: Actions
-    @IBAction func start(sender: UIButton) {
-        // EntityManager.sharedInstance.add(IoTFeverGame(username: playerNameText.text))
-
+    func getUsername(){
+        println("Get Username")
+        
+        var urlRequest = NSURLRequest(URL: NSURL(string: "http://192.168.1.32:1337/station/Vorto")!)
+        
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue(), completionHandler:{
+            (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            if let anError = error
+            {
+                // got an error in getting the data, need to handle it
+                println("error calling GET")
+            }
+            else // no error returned by URL request
+            {
+                // parse the result as json, since that's what the API provides
+                var jsonError: NSError?
+                let post = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as! NSDictionary
+                if let aJSONError = jsonError
+                {
+                    // got an error while parsing the data, need to handle it
+                    println("error parsing data")
+                }
+                else
+                {
+                    if var postUser = post["user"] as? NSDictionary
+                    {
+                        if var nickname = postUser["nickname"] as? String{
+                            println("The nickname is: " + nickname)
+                            
+                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                                username = nickname
+                                self.playerNameText.text = "Hi, " + username
+                                
+                                
+                                
+                            });
+                        }
+                        
+                    }
+                }
+            }
+        })
     }
+    
     
 }
 
